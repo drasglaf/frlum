08cb478e14b1576907eb432bf32bc6db49af5941
usuario <usuario@10.0.2.15>
2014-02-03 20:28:34 +0100

    Versión 2

---
+++ Proyecto1/src/HolaMundo.java
@@ -0,0 +1,18 @@
+public class HolaMundo {//definicion de la clase
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {//principio de main
+		//los corchetes definen que puede recibir valores o parametros(hoja)
+		// TODO Auto-generated method stub
+		int hora=8;
+		System.out.print("\r\t\tHola mundo son las "+hora);
+		
+		
+	}//final de main
+
+}
+/**todo programa java necesita una clase de metodo main
+*void significa que es un metodo que no va a devolver nada
+static método variable ligada invariablemente a la clase*/
---
+++ Proyecto1/src/HolaMundo_Hora.java
@@ -0,0 +1,23 @@
+public class HolaMundo_Hora {// definicion de la clase
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {// principio de main
+		// los corchetes definen que puede recibir valores o parametros(hoja)
+		// TODO Auto-generated method stub
+		int hora = 8;
+		System.out.println("Hola mundo son las " + hora);
+		System.out
+				.println("Los videojuegos no son más que un lavado de cerebro que nos hace"
+						+ "susceptibles de ser víctimas de lavados de cerebro tales como el que nos quiere hacer "
+						+ "creer que los videojuegos no son más que un lavado de cerebro");
+
+	}// final de main
+
+}
+/**
+ * todo programa java necesita una clase de metodo main void significa que es un
+ * metodo que no va a devolver nada static método variable ligada
+ * invariablemente a la clase
+ */
